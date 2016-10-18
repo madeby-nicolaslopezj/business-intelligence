97128c52bc2a6766709997399d00431bdf8eff13
Nicolás López <nicolaslopezj@me.com>
2016-10-17 21:12:11 -0300

    ready

--- 10-17/entrega/code.R
+++ 10-17/entrega/code.R
@@ -1,12 +1,20 @@
+# Load libraries
 library(flexclust)
+library(mclust)
+library(dplyr)
 
 # Import data
 data(Zoo, package = "seriation")
+
 # Show a summary of the data
 summary(Zoo)
 
-# Distance without class
+# Distance without class attribtue
 distance <- dist(Zoo[,-17], method = "euclidean")
+
+######
+# 1: Hierarchical clustering
+######
 
 bestMethod <- 'None'
 bestResult <- 0
@@ -21,4 +29,40 @@ for (method in c("single", "ward.D", "average", "complete", "centroid", "median"
   }
 }
 print(paste("Best method is", bestMethod, "with", bestResult))
+
+######
+# 2: GMM with EM algorithm
+######
+
+for (modelName in c("EII", "VII", "EEI", "EVI", "VEI", "VVI")){
+  model <- Mclust(Zoo, modelNames=modelName)
+  print(summary(model,data=Zoo))
+}
+# Best is VEI with -1411.076
+
+
+#####
+# 3: Compare Ward.D with VEI
+#####
+fit.ward <- hclust(distance, method='ward.D')
+groups.ward <- cutree(fit.ward, k=7)
+result.ward <- randIndex(groups.ward, Zoo[,17], correct=FALSE)
+
+fit.vei <- Mclust(Zoo, modelNames='VEI')
+groups.vei <- fit.vei$classification
+result.vei <- randIndex(groups.vei, Zoo[,17], correct=FALSE)
+
+if (result.ward > result.vei) {
+  print('Ward is better')
+} else {
+  print('VEI is better')
+}
+
+#####
+# 4: Redefine classes
+####
+
+plot(fit.ward)
+plot(fit.ward, label=Zoo$class)
+rect.hclust(fit.ward, k=7, border="red") 
 
